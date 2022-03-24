293b21499bdaba4bbbf6420f6dffca25ec89c78a
shwethab1999 <shwethab1275@gmail.com>
2022-03-24 13:11:20 +0530

    implement addition program

---
+++ addprogram/src/add.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"add.hpp"
+using namespace std;
+int add(int a, int b)
+{
+	int c=0;
+	c=a+b;
+	return c;
+}
+
---
+++ addprogram/src/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"add.hpp"
+using namespace std;
+int main()
+{
+	int a=10,b=5;
+        cout<<"sum is "<<add(a,b)<<endl;
+return 0;
+}
